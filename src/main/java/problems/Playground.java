64302a80e92217f0973d83d582552859469afdf4
Raul Maza <raul.mazaa@gmail.com>
2021-12-05 22:46:28 +0100

    .

--- src/main/java/problems/Playground.java
+++ src/main/java/problems/Playground.java
@@ -1,33 +1,24 @@
-package problems;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Playground {
 
     public static void main(String[] args) {
-        int [] arr = new int[] {1,1,3,2,3,3};
-        int sol = 0;
-        for (int i : arr) {
-            sol = sol ^ i;
-        }
-        System.out.println(sol);
+        System.out.println(solve(new int[]{1,7,3,4,10}));
+    }
 
-        HashMap<Integer, Integer> countMap = new HashMap<>();
-        for (int i : arr) {
-            if (!countMap.containsKey(i)) {
-                countMap.put(i, 1);
-            } else {
-                countMap.put(i, countMap.get(i));
+    public static int solve(int[] nums) {
+        int counter = 0;
+        int[] sortedArr = nums;
+        Arrays.sort(sortedArr);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == sortedArr[i]) {
+                counter++;
             }
         }
-
-        Map.Entry<Integer, Integer> majorityEntry = null;
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
-                majorityEntry = entry;
-            }
-        }
-        System.out.println(majorityEntry.getKey());
+        return counter;
     }
 }
+
+
+
+
