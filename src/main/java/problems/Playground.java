d84b34591227f3bfee30cace2e83935d77094cac
Raul Maza <raul.mazaa@gmail.com>
2021-12-06 17:17:39 +0100

    .

--- src/main/java/problems/Playground.java
+++ src/main/java/problems/Playground.java
@@ -3,19 +3,25 @@ import java.util.*;
 public class Playground {
 
     public static void main(String[] args) {
-        System.out.println(solve(new int[]{1,7,3,4,10}));
+//        System.out.println(solve([[1,4],[3,5],[6,9],7,9}}));
     }
 
-    public static int solve(int[] nums) {
-        int counter = 0;
-        int[] sortedArr = nums;
-        Arrays.sort(sortedArr);
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == sortedArr[i]) {
-                counter++;
+    public static int solve(int[][] intervals) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] arr : intervals) {
+            for (int i : arr) {
+                map.put(i, map.getOrDefault(i, 0) + 1);
             }
         }
-        return counter;
+        int mostFrequentNum = Integer.MAX_VALUE;
+        int frequency = 1;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > frequency) {
+                frequency = entry.getValue();
+                mostFrequentNum = Math.min(mostFrequentNum, entry.getKey());
+            }
+        }
+        return mostFrequentNum;
     }
 }
 
